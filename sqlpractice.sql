77adaff4935d2e57f89001197596ffa8d9b76e2c
Chelsey Branham <chelsbran@gmail.com>
2023-02-04 05:46:16 -0700

    left join

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -5,3 +5,12 @@ select *
 FROM Products
 LEFT JOIN Products
 ON OrderDetails.ProductID=Products.ProductID
+
+SELECT 
+t1.productID,
+t1.Quantity,
+t2.productName,
+t2.price
+FROM OrderDetails as t1
+LEFT JOIN Products as t2
+ON t1.ProductID=t2.ProductID
