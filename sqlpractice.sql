3f595563a9eab8e1903437d04282ba86a305a51f
Chelsey Branham <chelsbran@gmail.com>
2023-03-20 21:08:03 -0700

    sql practice

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -15,35 +15,3 @@ FROM OrderDetails as t1
 LEFT JOIN Products as t2
 ON t1.ProductID=t2.ProductID
 
-
-SELECT
-    first_name,
-    last_name
-FROM
-    sales.customers
-
-SELECT
-    product_name,
-    list_price
-FROM
-    production.products
-WHERE
-    list_price IN (89.99, 109.99, 159.99)
-ORDER BY
-    list_price;
-SELECT 
-InvoiceDate, 
-BillingAddress, 
-BillingCity,
-total
-FROM
-Invoice
-WHERE 
-total >0.99 AND BillingCity='Dublin'
-SELECT * FROM 
-Invoice AS i
-INNER JOIN
-Customer AS c
-ON
-i.CustomerId=c.CustomerId
-ORDER BY c.CustomerId
